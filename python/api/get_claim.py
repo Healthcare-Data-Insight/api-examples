c44c68b826608c7086aaab9bc93b71de44fb3b40
aananiev <alexander@datainsight.health>
2024-01-27 08:13:51 -0500

    Updated examples not to use the default dataset

--- python/api/get_claim.py
+++ python/api/get_claim.py
@@ -1,11 +1,21 @@
 import requests
-def _get_claim_by_pcn(base_url, pcn):
-    url = base_url+"/claims/default"
-    url = url+"?pcn="+pcn
+
+def get_claim_by_pcn(base_url, pcn):
+    url = base_url + "/claims"
+    url = url + "?pcn=" + pcn
     response = requests.get(url)
     return response.json()
 
-api_url="http://localhost:4080/clinsight/api"
-print('Started')
-claim_json=_get_claim_by_pcn(api_url, "125WILL")
-print(claim_json)
+def get_claim_by_id(base_url, id):
+    url = base_url + "/claims"
+    url = url + "/" + id
+    response = requests.get(url)
+    return response.json()
+
+api_url = "http://localhost:4080/clinsight/api"
+
+claim_json = get_claim_by_pcn(api_url, "125WILL")
+first_claim = claim_json[0]
+claim=get_claim_by_id(api_url, first_claim["id"])
+
+print(claim)
--- python/api/upload_files.py
+++ python/api/upload_files.py
@@ -1,30 +1,38 @@
 import requests
 
-def upload_files(base_url, file):
-    url = base_url + "/files/default"
-    print("Uploading {} to {} ".format(file, url))
-    files_dict = {"files": open(file, 'rb')}
-    file_upload_response=requests.post(url, files=files_dict)
+def rebuild_analytics(api_url):
+    api_url = api_url + "/analytics/rebuild"
+    # Don't block
+    api_url = api_url + "?async=true"
+    requests.post(api_url)
+
+def upload_files(api_url, file):
+    files_api_url = api_url + "/files"
+    print("Uploading {} to {} ".format(file, files_api_url))
+    file_upload_response = requests.post(files_api_url, files={"files": open(file, 'rb')})
+    # Rebuild analytics after the upload
+    rebuild_analytics(api_url)
     return file_upload_response.json()
 
+
 def fetch_claims_for_file_url(api_url, file_url):
-    api_url = api_url + "/claims/default"
-    api_url = api_url+"?fileUrl="+file_url
+    api_url = api_url + "/claims"
+    api_url = api_url + "?fileUrl=" + file_url
     # TODO: use pagination
     return requests.get(api_url).json()
+
 
 clinsight_api_url = "http://localhost:4080/clinsight/api"
 test_files_dir = "../../sample_files"
 test_file = test_files_dir + "/837/prof-encounter.dat"
 
-upload_info=upload_files(clinsight_api_url, test_file)
-file_infos=upload_info["files"]
+upload_info = upload_files(clinsight_api_url, test_file)
+file_infos = upload_info["files"]
 for file_info in file_infos:
-    claims=fetch_claims_for_file_url(clinsight_api_url, file_info["url"])
+    claims = fetch_claims_for_file_url(clinsight_api_url, file_info["url"])
     # Do something useful with our claims
     for claim in claims:
-        pcn=claim["patientControlNumber"]
-        charge=claim["chargeAmount"]
-        billing_npi=claim["billingProvider"]["identifier"]
+        pcn = claim["patientControlNumber"]
+        charge = claim["chargeAmount"]
+        billing_npi = claim["billingProvider"]["identifier"]
         print("Claim {} from {} for the amount {}".format(pcn, billing_npi, charge))
-
