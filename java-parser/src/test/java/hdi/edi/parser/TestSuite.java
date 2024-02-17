fef69f633cc6181400d7377b5e812ffe0f4ed572
aananiev <alexander@datainsight.health>
2024-02-17 08:06:17 -0500

    Updated logback config and version number

--- java-parser/src/test/java/hdi/edi/parser/TestSuite.java
+++ java-parser/src/test/java/hdi/edi/parser/TestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         ClaimParsingExample.class,
+        PaymentParsingExample.class,
         SegmentParsingExample.class
 })
 public class TestSuite {
