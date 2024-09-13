36973dbb05a4e3db6e7d159a30f076b0af284b13
aananiev <alexander@datainsight.health>
2024-09-13 14:34:35 -0400

    Updated CURL examples

--- curl/convert_edi.sh
+++ curl/convert_edi.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
-export API_URL=http://localhost:5080/clinsight/api
-
+export API_URL=http://localhost:4080/clinsight/api
+echo -- Upload multiple files and convert to JSON
 # Upload multiple files and get json back as ndjson
-curl -X POST -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" ${API_URL}/edi/json/upload?splitTran=true&ndjson=false
+curl -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" ${API_URL}/edi/json/upload?splitTran=true&ndjson=false
 # Or as regular JSON with array of claims
-curl -X POST -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" ${API_URL}/edi/json/upload?splitTran=true
+curl -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" ${API_URL}/edi/json/upload?splitTran=true
 
-# Upload multiple files and get CSV back
-curl -X POST -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" ${API_URL}/edi/csv/upload
+echo -- Post single file
+# Post content as a single file
+curl -X POST -H "Content-Type: text/plain" --data-binary @../edi_files/837/prof-encounter.dat ${API_URL}/edi/json&splitTran=true
+
+echo -- Upload multiple files and convert to CSV
+# Same for CSV conversion, upload files and get CSV back
+curl -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" ${API_URL}/edi/csv/upload
