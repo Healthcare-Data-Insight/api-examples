87559c63fc22f8b8342472d0f4700e7266f3cf40
aananiev <alexander@datainsight.health>
2025-04-17 14:42:24 -0400

    Updated docker image version

--- install/docker/run_ediconvert.sh
+++ install/docker/run_ediconvert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-docker run -it --rm --name ediconvert  -p "5080:5080" -v ./etc:/app/etc repo.datainsight.health/ediconvert:2.11
+docker run -it --rm --name ediconvert  -p "5080:5080" -v ./etc:/app/etc repo.datainsight.health/ediconvert:2.12
