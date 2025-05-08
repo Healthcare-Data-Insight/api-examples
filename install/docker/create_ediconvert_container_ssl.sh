02eab76e39e0e315eb52e2da7f0040b867c98f85
aananiev <alexander@datainsight.health>
2025-05-08 11:01:41 -0400

    Updated ediconvert image version

--- install/docker/create_ediconvert_container_ssl.sh
+++ install/docker/create_ediconvert_container_ssl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-docker create --name ediconvert  -p "8443:5080" -v ./etc:/app/etc -e SERVER_SSL_CERTIFICATE=./etc/ssl/certificate.crt -e SERVER_SSL_CERTIFICATEPRIVATEKEY=./etc/ssl/private.key repo.datainsight.health/ediconvert:2.12
+docker create --name ediconvert  -p "8443:5080" -v ./etc:/app/etc -e SERVER_SSL_CERTIFICATE=./etc/ssl/certificate.crt -e SERVER_SSL_CERTIFICATEPRIVATEKEY=./etc/ssl/private.key repo.datainsight.health/ediconvert:2.13
