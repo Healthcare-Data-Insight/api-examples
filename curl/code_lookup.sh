0f7fc027e54963ace49866269549144104f7c81c
aananiev <alexander@datainsight.health>
2025-06-04 11:45:46 -0400

    Updated 834 examples

--- curl/code_lookup.sh
+++ curl/code_lookup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -e
 export API_URL=https://datainsight.health/clinsight/api
-export API_KEY="12345"
+export API_KEY="your-api-key"
 # * code search
 # Simple search
-curl -H "X-Api-Key: $API_KEY" $API_URL/code/search?query=0LBQ4
+curl -H "X-Api-Key: $API_KEY" "$API_URL/code/search?query=0LBQ4"
 # By words in the description
-curl -H "X-Api-Key: $API_KEY" -G $API_URL/code/search?type=procedure --data-urlencode 'query=endo right knee'
+curl -H "X-Api-Key: $API_KEY" -G "$API_URL/code/search?type=procedure" --data-urlencode 'query=endo right knee'
 # Multiple codes
-curl -H "X-Api-Key: $API_KEY" -G $API_URL/code/search?type=procedure --data-urlencode 'query=0LBQ4, 0Y3F4ZZ'
+curl -H "X-Api-Key: $API_KEY" -G "$API_URL/code/search?type=procedure" --data-urlencode 'query=0LBQ4, 0Y3F4ZZ'
 # Initial code letter + description
-curl -H "X-Api-Key: $API_KEY" -G $API_URL/code/search?type=procedure --data-urlencode 'query=code:j adrenalin'
+curl -H "X-Api-Key: $API_KEY" -G "$API_URL/code/search?type=procedure" --data-urlencode 'query=code:j adrenalin'
 
 # * CSV export
 # All procedures
-curl -H "X-Api-Key: $API_KEY" -G $API_URL/code/csv?type=procedure
+curl -H "X-Api-Key: $API_KEY" -G "$API_URL/code/csv?type=procedure"
 # Procedures for the search query
-curl -H "X-Api-Key: $API_KEY" -G $API_URL/code/csv?type=procedure --data-urlencode 'query=0LBQ4, 0Y3F4ZZ'
+curl -H "X-Api-Key: $API_KEY" -G "$API_URL/code/csv?type=procedure" --data-urlencode 'query=0LBQ4, 0Y3F4ZZ'
