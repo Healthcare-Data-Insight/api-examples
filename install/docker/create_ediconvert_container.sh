fcdf7af87138c0b0d286f421f614069043b1c764
aananiev <alexander@datainsight.health>
2025-09-28 10:53:09 -0400

    Moves the logic for adding statuses to results

--- install/docker/create_ediconvert_container.sh
+++ install/docker/create_ediconvert_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-docker create --name ediconvert  -p "5080:5080" -v ./etc:/app/etc repo.datainsight.health/ediconvert:2.13
+docker create --name ediconvert  -p "5080:5080" -v ./etc:/app/etc repo.datainsight.health/ediconvert:2.14
--- install/docker/run_ediconvert.sh
+++ install/docker/run_ediconvert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-docker run -it --rm --name ediconvert  -p "5080:5080" -v ./etc:/app/etc repo.datainsight.health/ediconvert:2.13
+docker run -it --rm --name ediconvert  -p "5080:5080" -v ./etc:/app/etc repo.datainsight.health/ediconvert:2.14
