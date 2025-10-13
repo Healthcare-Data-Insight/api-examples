efe89265cc5a59961734e416a1cec6e32c9510d5
aananiev <alexander@datainsight.health>
2025-10-13 11:30:43 -0400

    Fine-tuned dependencies

--- java-parser/src/test/java/hdi/edi/parser/TestSuite.java
+++ java-parser/src/test/java/hdi/edi/parser/TestSuite.java
@@ -2,6 +2,7 @@ package hdi.edi.parser;
 
 
 import hdi.edi.parser.edi810.Edi810ParsingExample;
+import hdi.edi.writer.ClaimToEdiExample;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -13,7 +14,8 @@ import org.junit.runners.Suite;
         Member834ParsingExample.class,
         ClaimStatus277CAParsingExample.class,
         SegmentParsingExample.class,
-        Edi810ParsingExample.class
+        Edi810ParsingExample.class,
+        ClaimToEdiExample.class
 })
 
 public class TestSuite {
