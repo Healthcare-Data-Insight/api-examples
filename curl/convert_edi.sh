95962f61168b98b2ea2ae10748b3bcacca93c6d7
aananiev <alexander@datainsight.health>
2025-10-28 19:06:10 -0400

    Updated converted files examples

--- curl/convert_edi.sh
+++ curl/convert_edi.sh
@@ -7,16 +7,16 @@ curl $API_URL/about
 
 echo -- Upload multiple files and convert to JSON
 # Upload multiple files and get json back as ndjson
-curl -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" "$API_URL/edi/json?splitTran=true&ndjson=true"
+curl -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" "$API_URL/edi/json?ndjson=true"
 # Or as regular JSON with array of claims
-curl -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" "$API_URL/edi/json?splitTran=true"
+curl -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" "$API_URL/edi/json"
 
 echo -- Post single file
 # Post content of 837 as a single file
-curl -H "Content-Type: text/plain" --data-binary @../edi_files/837/prof-encounter.dat "$API_URL/edi/json?splitTran=true&ediFileName=prof-encounter.dat"
+curl -H "Content-Type: text/plain" --data-binary @../edi_files/837/prof-encounter.dat "$API_URL/edi/json?ediFileName=prof-encounter.dat"
 
 # Post content of 835 as a single file
-curl -H "Content-Type: text/plain" --data-binary @../edi_files/835/negotiated_discount.dat "$API_URL/edi/json?splitTran=true&ediFileName=negotiated_discount.dat"
+curl -H "Content-Type: text/plain" --data-binary @../edi_files/835/negotiated_discount.dat "$API_URL/edi/json?ediFileName=negotiated_discount.dat"
 
 echo -- Upload multiple files and convert to CSV
 # Same for CSV conversion, upload files and get CSV back. splitTran=true is the default for CSV, no need to provide it
