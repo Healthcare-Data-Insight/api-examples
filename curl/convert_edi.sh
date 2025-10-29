fd23e253ba197b8dd4226a6fff6f3d0b92fd7a00
aananiev <alexander@datainsight.health>
2025-10-28 22:19:43 -0400

    Updated CURL examples

--- curl/convert_edi.sh
+++ curl/convert_edi.sh
@@ -4,27 +4,27 @@ export API_URL=http://localhost:5080/api
 
 # get the app version and the license info
 curl $API_URL/about
-
+echo
 echo -- Upload multiple files and convert to JSON
 # Upload multiple files and get json back as ndjson
-curl -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" "$API_URL/edi/json?ndjson=true"
+curl -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" "$API_URL/edi/json?ndjson=true" > 837p.ndjson
 # Or as regular JSON with array of claims
-curl -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" "$API_URL/edi/json"
+curl -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" "$API_URL/edi/json" > 837p.json
 
 echo -- Post single file
 # Post content of 837 as a single file
 curl -H "Content-Type: text/plain" --data-binary @../edi_files/837/prof-encounter.dat "$API_URL/edi/json?ediFileName=prof-encounter.dat"
 
 # Post content of 835 as a single file
-curl -H "Content-Type: text/plain" --data-binary @../edi_files/835/negotiated_discount.dat "$API_URL/edi/json?ediFileName=negotiated_discount.dat"
+curl -H "Content-Type: text/plain" --data-binary @../edi_files/835/835-all-fields.dat "$API_URL/edi/json?ediFileName=835-all-fields.dat" > 835.json
 
 echo -- Upload multiple files and convert to CSV
-# Same for CSV conversion, upload files and get CSV back. splitTran=true is the default for CSV, no need to provide it
-curl -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" "$API_URL/edi/csv"
+# Same for CSV conversion, upload files and get CSV back
+curl -F files=@"../edi_files/837/prof-encounter.dat" -F files=@"../edi_files/837/anesthesia.dat" "$API_URL/edi/csv" > 837p.csv
 curl -H "Content-Type: text/plain" --data-binary @../edi_files/837/prof-encounter.dat "$API_URL/edi/csv?ediFileName=prof-encounter.dat"
 
 # Convert 277
-curl -H "Content-Type: text/plain" --data-binary @../edi_files/277/277CA-all-fields.edi "$API_URL/edi/json"
+curl -H "Content-Type: text/plain" --data-binary @../edi_files/277/277CA-all-fields.edi "$API_URL/edi/json" > 277ca.json
 
 # NCPDP
 echo -- Parse NCPDP telco b1 file
