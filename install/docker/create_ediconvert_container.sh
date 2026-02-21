e0d5bd1065a467ccb79fd7c71637a13b577676aa
aananiev <alexander@datainsight.health>
2026-02-21 17:24:50 -0500

    Updated container startup scripts

--- install/docker/create_ediconvert_container.sh
+++ install/docker/create_ediconvert_container.sh
@@ -3,4 +3,4 @@ set -e
 # Remove the container if already exists
 docker rm -f ediconvert 2>/dev/null || true
 docker create --name ediconvert  --restart unless-stopped -p "5080:5080" -v ./etc:/app/etc:ro repo.datainsight.health/ediconvert:2.14
-# Now run docker start -ai ediconvert
+docker start -ai ediconvert
--- install/docker/create_ediconvert_container_ssl.sh
+++ install/docker/create_ediconvert_container_ssl.sh
@@ -2,6 +2,6 @@
 set -e
 # Remove the container if already exists
 docker rm -f ediconvert 2>/dev/null || true
-docker create --name ediconvert --restart unless-stopped -p "8443:5080" -v ./etc:/app/etc:ro -e SERVER_SSL_CERTIFICATE=./etc/ssl/certificate.crt -e SERVER_SSL_CERTIFICATEPRIVATEKEY=./etc/ssl/private.key repo.datainsight.health/ediconvert:2.14
-# Now run docker start -ai ediconvert
 # The container listens on 8443 port
+docker create --name ediconvert --restart unless-stopped -p "8443:5080" -v ./etc:/app/etc:ro -e SERVER_SSL_CERTIFICATE=./etc/ssl/certificate.crt -e SERVER_SSL_CERTIFICATEPRIVATEKEY=./etc/ssl/private.key repo.datainsight.health/ediconvert:2.14
+docker start -ai ediconvert
