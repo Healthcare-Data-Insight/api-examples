a51db5c39e06d441885d6c2d2a47dce6c240da33
aananiev <alexander@datainsight.health>
2026-03-10 14:37:33 -0400

    Split lambda into two separate functions

---
+++ install/terraform/lambda/ediconvert-lambda-trigger.tf
@@ -0,0 +1,63 @@
+/*
+EDI Converter Lambda function with a trigger on an input S3 bucket.
+*/
+resource "aws_lambda_function" "edi_converter_function_trigger" {
+  environment {
+    variables = {
+      # Replace with your key, the key below has expired
+      EDI_LICENSE_KEY = "Ic5OXgAAABoAAAACAAAACwAAAANlbnRpdGxlbWVudEVESQAAABoAAAALAAAACmV4cGlyYXRpb24AAAGdIyY2AAAAAJwAAAABAAAAEAAAAIBsaWNlbnNlU2lnbmF0dXJlNNiFGndeVeM9X4kxO9SFf7U0Gq7K9LLKEKPEhW5TTqZvFEB2QyKb1A7/8BjddgIZUAnNxRSykCMj6u34YlBeibVwkHqyg2p31qHRwYGtVO4O6YTEsveZ15gH5yOS1vZI6ztxI7MWsOXn7bupiezerY4/0MLMHuWqC6V1+kQVVLQAAAAiAAAAAgAAAA8AAAAHc2lnbmF0dXJlRGlnZXN0U0hBLTUxMg=="
+      # Include parser's warnings in the output
+      OUTPUT_WARNINGS = "True"
+      # name of the output bucket
+      OUT_BUCKET      = "edi-out"
+      # JSON, JSONL, CSV, EXCEL
+      OUT_FORMAT      = "JSON"
+    }
+  }
+
+  ephemeral_storage {
+    size = "1024"
+  }
+
+  function_name = "EdiConverterFromTrigger"
+  # Handler for the trigger
+  handler       = "hdi.aws.ConverterTriggerEventHandler::handleRequest"
+
+  # The code is deployed from this public bucket
+  s3_bucket     = "ediconverter"
+  s3_key        = "ediconvert-lambda-2.14.zip"
+
+  memory_size = "512"
+  package_type = "Zip"
+  role         = aws_iam_role.lambda_exec_role.arn
+  runtime      = "java25"
+
+  # Increase timeout if processing large files
+  timeout = "180"
+}
+
+resource "aws_s3_bucket" "edi_in_bucket" {
+  bucket = "edi-in"
+}
+
+resource "aws_s3_bucket_notification" "s3_lambda_trigger" {
+  bucket = aws_s3_bucket.edi_in_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.edi_converter_function_trigger.arn
+    events             = ["s3:ObjectCreated:*"]
+    filter_prefix      = ""
+    filter_suffix      = ""
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3_invoke]
+}
+
+resource "aws_lambda_permission" "allow_s3_invoke" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.edi_converter_function_trigger.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.edi_in_bucket.arn
+}
+
--- install/terraform/lambda/main.tf
+++ install/terraform/lambda/main.tf
@@ -1,6 +1,6 @@
 /*
-EDI Converter Lambda function with a trigger on an input S3 bucket
- */
+Shared Terraform resources used by both lambda variants.
+*/
 
 terraform {
   required_providers {
@@ -15,66 +15,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-resource "aws_lambda_function" "edi_converter_function" {
-
-  environment {
-    variables = {
-      # Replace with your key, the key below has expired
-      EDI_LICENSE_KEY = "Ic5OXgAAABoAAAACAAAACwAAAANlbnRpdGxlbWVudEVESQAAABoAAAALAAAACmV4cGlyYXRpb24AAAGadm9YgAAAAJwAAAABAAAAEAAAAIBsaWNlbnNlU2lnbmF0dXJlRsqEQItRpwCXDXeor0dJOtbLApxQmJ7A2hzUY2jdzTncOO0K5Gy/U0DIdqXhYwRcVzo4KipXAF70sObluAjTQSvBc17DMRqh2NjER0JJV5+/KjROS/JHzyhRIGvlUB0WH11scrEDLhuGndI5dEk/v6sVgA8G7hqfINkW9BMt4V0AAAAiAAAAAgAAAA8AAAAHc2lnbmF0dXJlRGlnZXN0U0hBLTUxMg=="
-      # Include parser's warnings in the output
-      OUTPUT_WARNINGS = "True"
-      # name of the output bucket
-      OUT_BUCKET      = "ediout"
-      # JSON, JSONL, CSV, EXCEL
-      OUT_FORMAT      = "JSON"
-    }
-  }
-
-  # Increase storage if processing large files
-  ephemeral_storage {
-    size = "1024"
-  }
-
-  function_name = "EdiConverter"
-  # Handler for the trigger
-  handler       = "hdi.aws.ConverterTriggerEventHandler::handleRequest"
-  # Handler for direct events
-  #handler       = "hdi.aws.ConverterEventHandler::handleRequest"
-
-  # The code is deployed from this public bucket
-  s3_bucket     = "ediconverter"
-  s3_key        = "ediconvert-lambda-2.14.zip"
-
-  memory_size                    = "512"
-  package_type                   = "Zip"
-  role                           = aws_iam_role.lambda_exec_role.arn
-  runtime                        = "java25"
-
-  # Increase timeout if processing large files
-  timeout = "180"
-
-}
-
-# Your input bucket with EDI files for the trigger
-resource "aws_s3_bucket" "edi_in_bucket" {
-  bucket = "edi-in"
-}
-
-# Trigger on the input bucket
-resource "aws_s3_bucket_notification" "s3_lambda_trigger" {
-  bucket = aws_s3_bucket.edi_in_bucket.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.edi_converter_function.arn
-    events = ["s3:ObjectCreated:*"]
-    filter_prefix       = ""
-    filter_suffix       = ""
-  }
-  depends_on = [aws_lambda_permission.allow_s3_invoke]
-}
-
-# Lambda function role
+# Shared lambda execution role
 resource "aws_iam_role" "lambda_exec_role" {
   name = "java_lambda_execution_role"
 
@@ -92,12 +33,10 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 }
 
-# IAM policy allowing full access to all S3 buckets in your account
-# Customize it to only include the buckets that you use
-# The function reads from input bucket and outputs to another bucket
+# Shared IAM policy for S3 access
 resource "aws_iam_policy" "lambda_s3_access_policy" {
   name        = "lambda_s3_full_access"
-  description = "Lambda function can access all S3 buckets"
+  description = "Lambda functions can access all S3 buckets"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -129,10 +68,11 @@ resource "aws_iam_role_policy_attachment" "lambda_s3" {
   policy_arn = aws_iam_policy.lambda_s3_access_policy.arn
 }
 
-resource "aws_lambda_permission" "allow_s3_invoke" {
-  statement_id  = "AllowS3Invoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.edi_converter_function.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.edi_in_bucket.arn
+output "edi_converter_trigger_function_name" {
+  value = aws_lambda_function.edi_converter_function_trigger.function_name
 }
+
+output "edi_converter_manual_function_name" {
+  value = aws_lambda_function.edi_converter_function_manual.function_name
+}
+
