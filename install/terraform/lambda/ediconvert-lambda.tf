d2fc7409847cd08f58ccd72c6efb3f08941b576b
aananiev <alexander@datainsight.health>
2026-03-10 15:33:42 -0400

    Updated Lambda terraform files to create two separate files for the event-based function and for the trigger-based function

--- install/terraform/lambda/ediconvert-lambda.tf
+++ install/terraform/lambda/ediconvert-lambda.tf
@@ -1,6 +1,20 @@
 /*
 EDI Converter Lambda function without an S3 trigger for manual invocation.
 */
+variable "run_test_events" {
+  description = "Set to true to run Lambda test invocations from the events directory"
+  type        = bool
+  default     = false
+}
+
+locals {
+  # Load test payload files for manual Lambda invocations
+  manual_lambda_event_paths = sort(fileset("${path.module}/events", "*.json"))
+  manual_lambda_test_events = [
+    for event_path in local.manual_lambda_event_paths : jsondecode(file("${path.module}/events/${event_path}"))
+  ]
+}
+
 resource "aws_lambda_function" "edi_converter_function_manual" {
   environment {
     variables = local.edi_converter_lambda_environment
@@ -31,4 +45,15 @@ resource "aws_lambda_function" "edi_converter_function_manual" {
   snap_start {
     apply_on = "PublishedVersions"
   }
+}
+# Run test events, only if run_test_events is True
+resource "aws_lambda_invocation" "edi_converter_manual_test_events" {
+  for_each = var.run_test_events ? {
+    for idx, event_payload in local.manual_lambda_test_events : idx => event_payload
+  } : {}
+
+  function_name = aws_lambda_function.edi_converter_function_manual.function_name
+  input         = jsonencode(each.value)
+
+  depends_on = [aws_lambda_function.edi_converter_function_manual]
 }
