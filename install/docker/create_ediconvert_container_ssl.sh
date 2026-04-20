797cb073846dd0e8316fc743369381b2e5b99e36
aananiev <alexander@datainsight.health>
2026-04-20 17:55:14 -0400

    Updated docker image version to 2.15

--- install/docker/create_ediconvert_container_ssl.sh
+++ install/docker/create_ediconvert_container_ssl.sh
@@ -3,5 +3,5 @@ set -e
 # Remove the container if already exists
 docker rm -f ediconvert 2>/dev/null || true
 # The container listens on 8443 port
-docker create --name ediconvert --restart unless-stopped -p "8443:5080" -v ./etc:/app/etc:ro -e SERVER_SSL_CERTIFICATE=./etc/ssl/certificate.crt -e SERVER_SSL_CERTIFICATEPRIVATEKEY=./etc/ssl/private.key repo.datainsight.health/ediconvert:2.14
+docker create --name ediconvert --restart unless-stopped -p "8443:5080" -v ./etc:/app/etc:ro -e SERVER_SSL_CERTIFICATE=./etc/ssl/certificate.crt -e SERVER_SSL_CERTIFICATEPRIVATEKEY=./etc/ssl/private.key repo.datainsight.health/ediconvert:2.15
 docker start -ai ediconvert
--- install/docker/run_ediconvert.sh
+++ install/docker/run_ediconvert.sh
@@ -3,4 +3,4 @@ set -e
 # Remove the container if already exists
 docker rm -f ediconvert 2>/dev/null || true
 # Run the container, attach to it and remove after the shutdown
-docker run -it --rm --name ediconvert  -p "5080:5080" -v ./etc:/app/etc:ro repo.datainsight.health/ediconvert:2.14
+docker run -it --rm --name ediconvert  -p "5080:5080" -v ./etc:/app/etc:ro repo.datainsight.health/ediconvert:2.15
