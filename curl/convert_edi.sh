8695a98635d0fdfea169b9393e32a5dada095a30
aananiev <alexander@datainsight.health>
2026-05-26 21:06:50 -0400

    Added validation curl examples for public API

--- curl/convert_edi.sh
+++ curl/convert_edi.sh
@@ -26,6 +26,7 @@ curl -H "Content-Type: text/plain" --data-binary @../edi_files/837/prof-encounte
 # Convert 277
 curl -H "Content-Type: text/plain" --data-binary @../edi_files/277/277CA-all-fields.edi "$API_URL/edi/json?validate=true" > 277ca.json
 
+echo -- Validate EDI files
 # Validate various EDI files
 curl -H "Content-Type: text/plain" --data-binary @../edi_files/835/835-validation-issues.edi "$API_URL/edi/validate"
 curl -H "Content-Type: text/plain" --data-binary @../edi_files/837/837P-validation-issues.edi "$API_URL/edi/validate"
@@ -35,3 +36,9 @@ curl -H "Content-Type: text/plain" --data-binary @../edi_files/837/837P-validati
 # NCPDP
 echo -- Parse NCPDP telco b1 file
 curl -H "Content-Type: text/plain" --data-binary @../edi_files/ncpdp/b1_telco.dat "$API_URL/ncpdp/parse"
+
+# Validate using public API
+echo $'\n-- Validate EDI files using public API'
+# Your license key can be used as the API key, request your license key at https://datainsight.health/products/edi-license/
+export API_KEY="Ic5OXgAAABoAAAACAAAACwAAAANlbnRpdGxlbWVudEVESQAAABoAAAALAAAACmV4cGlyYXRpb24AAAGdIyY2AAAAAJwAAAABAAAAEAAAAIBsaWNlbnNlU2lnbmF0dXJlNNiFGndeVeM9X4kxO9SFf7U0Gq7K9LLKEKPEhW5TTqZvFEB2QyKb1A7/8BjddgIZUAnNxRSykCMj6u34YlBeibVwkHqyg2p31qHRwYGtVO4O6YTEsveZ15gH5yOS1vZI6ztxI7MWsOXn7bupiezerY4/0MLMHuWqC6V1+kQVVLQAAAAiAAAAAgAAAA8AAAAHc2lnbmF0dXJlRGlnZXN0U0hBLTUxMg=="
+curl -H "Content-Type: text/plain" -H "X-Api-Key: $API_KEY" --data-binary @../edi_files/837/837P-validation-issues.edi "https://datainsight.health/clinsight/api/edi/validate"
