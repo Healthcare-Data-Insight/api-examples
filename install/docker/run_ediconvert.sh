a0efb38867dba1df9e4c946bb751698be61eda04
aananiev <alexander@datainsight.health>
2026-06-18 22:48:57 -0400

    Updated the version number

--- install/docker/run_ediconvert.sh
+++ install/docker/run_ediconvert.sh
@@ -3,4 +3,4 @@ set -e
 # Remove the container if already exists
 docker rm -f ediconvert 2>/dev/null || true
 # Run the container, attach to it and remove after the shutdown
-docker run -it --rm --name ediconvert  -p "5080:5080" -v ./etc:/app/etc:ro myarch/ediconvert:2.15.0-beta
+docker run -it --rm --name ediconvert  -p "5080:5080" -v ./etc:/app/etc:ro myarch/ediconvert:2.15.0
--- install/terraform/lambda/ediconvert-lambda-trigger.tf
+++ install/terraform/lambda/ediconvert-lambda-trigger.tf
@@ -16,7 +16,7 @@ resource "aws_lambda_function" "edi_converter_function_trigger" {
 
   # The code is deployed from this public bucket
   s3_bucket     = "ediconverter"
-  s3_key        = "ediconvert-lambda-2.15.0-beta.zip"
+  s3_key        = "ediconvert-lambda-2.15.0.zip"
 
   memory_size = "512"
   package_type = "Zip"
