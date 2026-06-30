126050b4253e20133c3e44032bf9c5b87b367cdf
aananiev <alexander@datainsight.health>
2026-06-30 16:11:45 -0400

    Updated curl examples

--- curl/generate_edi.sh
+++ curl/generate_edi.sh
@@ -2,10 +2,11 @@
 set -e
 export API_URL=http://localhost:5080/api
 
-echo -- EDI for a minimalilstic 837P claim
-curl  -H "Content-Type: application/json" --data-binary @../edi_gen/request/837p-minimal.json $API_URL/edi/gen/claim
+echo -- Generate 837P EDI
+curl  -H "Content-Type: application/json" --data-binary @../edi_gen/request/837p-minimal.json $API_URL/edi/gen/837
 
-echo -- Validate our request to make sure the resulting EDI is valid
-curl  -H "Content-Type: application/json" --data-binary @../edi_gen/request/837p-minimal.json $API_URL/edi/gen/claim/validate
+echo $'\n-- Generate 835 EDI'
+curl  -H "Content-Type: application/json" --data-binary @../edi_gen/request/835-minimal.json $API_URL/edi/gen/835
 
-# Issues
+echo $'\n-- Validate our request to make sure the resulting EDI will be valid'
+curl  -H "Content-Type: application/json" --data-binary @../edi_gen/request/837p-minimal.json $API_URL/edi/gen/837/validate
