0d41b9d47b6952ae9b2397cefce10a9ecfd62564
aananiev <alexander@datainsight.health>
2026-07-13 12:14:30 -0400

    Updated 834 parsing example

--- java-parser/src/test/java/hdi/edi/parser/Member834ParsingExample.java
+++ java-parser/src/test/java/hdi/edi/parser/Member834ParsingExample.java
@@ -1,6 +1,9 @@
 package hdi.edi.parser;
 
+import hdi.edi.EdiTransaction;
 import hdi.edi.validation.ValidationIssue;
+import hdi.model.control.FunctionalGroup;
+import hdi.model.control.InterchangeControl;
 import hdi.model.coverage.HealthCoverage;
 import hdi.model.coverage.Member;
 import hdi.model.coverage.MemberCoverage;
@@ -8,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.io.File;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -18,21 +22,35 @@ public class Member834ParsingExample implements ParsingExampleHelper {
     @Test
     public void parse834() {
         var ediFile834 = new File(EDI_FILES_DIR + "/834/834-all-fields.edi");
-        List<MemberCoverage> memberCoverages;
         try (var parser = new EdiParser(ediFile834)
                 .isValidationMode(true)) {
             EdiParsingResults parsingResults;
             do {
                 // parse 20 members at a time
                 parsingResults = parser.parse(DEFAULT_CHUNK_SIZE);
-                memberCoverages = parsingResults.memberCoverages();
-                for (var memberCoverage : memberCoverages) {
-                    processMemberCoverage(memberCoverage);
-                }
-                // Validation issues at the transaction level
-                var issues = parsingResults.validationIssues();
-                for (var issue : issues) {
-                    log.warn("Validation issue: {}", issue);
+                // "rootObjs" contains all objects parsed from EDI in the order they appear in the EDI file
+                // process all transactions, members, and control segments
+                for (var rootObj : parsingResults.rootObjs()) {
+                    // MemberCoverage represents INS segment and all related segments
+                    if (rootObj instanceof MemberCoverage memberCoverage) {
+                        processMemberCoverage(memberCoverage);
+                    }
+                    else if (rootObj instanceof EdiTransaction transaction && transaction.transactionType() == TransactionType.MEMBER_COVERAGE) {
+                        process834Transaction(transaction);
+                    }
+                    else if (rootObj instanceof InterchangeControl interchangeControl) {
+                        log.info("ISA segment info:\n{}", interchangeControl);
+                    }
+                    else if (rootObj instanceof FunctionalGroup functionalGroup) {
+                        log.info("GS segment info:\n{}", functionalGroup);
+                    }
+                    // validation issue at the transaction level
+                    else if (rootObj instanceof ValidationIssue validationIssue) {
+                        // validation issues are logged by the parser; here you can do additional processing
+                    }
+                    else {
+                        throw new IllegalStateException("Unexpected object for 837 transaction: " + rootObj);
+                    }
                 }
             } while (!parsingResults.isDone());
         }
@@ -55,6 +73,12 @@ public class Member834ParsingExample implements ParsingExampleHelper {
         }
         // Validation issues for this member
         processValidationIssues(memberCoverage, memberCoverage.validationIssues());
+    }
+
+    private void process834Transaction(EdiTransaction transaction) {
+        String controlNumber = transaction.controlNumber();
+        LocalDateTime transactionDateTime = transaction.getCreationDateTime();
+        log.info("Transaction: {} {}", controlNumber, transactionDateTime);
     }
 
     // All validations are logged automatically, here you can do additional processing
